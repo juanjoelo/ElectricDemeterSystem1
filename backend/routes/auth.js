b7001af54cab21b0e121ee8105ce7aa71df9e9f9
juanjoelo <juanjoel.olguin@gmail.com>
2024-07-01 16:33:15 -0300

    update del maldito existingUser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User.js"); // Asegúrate de que esta ruta sea correcta
 // Ruta para registrar un nuevo usuario
+// Ruta para registrar un nuevo usuario
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
     // Verifica si ya existe un usuario con el mismo nombre de usuario o correo electrónico
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-    if (existingUser) {
+    const existingUser = await User.find({ $or: [{ username }, { email }] });
+    if (existingUser.length > 0) {
       return res.status(400).json({
         error: "Nombre de usuario o correo electrónico ya está en uso",
       });
